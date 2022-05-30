c9f5373c3c6f583a04aeaca49168b4d5533edbba
dlrwtul <dibrahimalang@gmail.com>
2022-05-30 15:22:04 +0000

    first

---
+++ src/Entity/Classe.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClasseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClasseRepository::class)]
+class Classe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 25)]
+    private $libelle;
+
+    #[ORM\Column(type: 'string', length: 25)]
+    private $filiere;
+
+    #[ORM\Column(type: 'string', length: 25)]
+    private $niveau;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getFiliere(): ?string
+    {
+        return $this->filiere;
+    }
+
+    public function setFiliere(string $filiere): self
+    {
+        $this->filiere = $filiere;
+
+        return $this;
+    }
+
+    public function getNiveau(): ?string
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau(string $niveau): self
+    {
+        $this->niveau = $niveau;
+
+        return $this;
+    }
+}
